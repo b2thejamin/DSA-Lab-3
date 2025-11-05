acf967d4f7bc75baf7d1d8e1ff8435bb7a01aaee
b2thejamin <157675845+b2thejamin@users.noreply.github.com>
2025-11-05 18:25:04 -0500

    Lab 3 Finished

--- Lab 3 - DList/DList.h
+++ Lab 3 - DList/DList.h
@@ -33,31 +33,31 @@ NOTE: If the unit test is not on, that code will not be compiled!
 */
 
 // Individual unit test toggles			
-#define LAB3_CTOR						0
-#define LAB3_NODE_CTOR_DEFAULT			0
-#define LAB3_NODE_CTOR					0
-#define LAB3_ADDHEAD_EMPTY				0
-#define LAB3_ADDHEAD					0
-#define LAB3_ADDTAIL_EMPTY				0
-#define LAB3_ADDTAIL					0
-#define LAB3_CLEAR						0
-#define LAB3_DTOR						0
-#define LAB3_ITER_BEGIN					0
-#define LAB3_ITER_END					0
-#define LAB3_ITER_INCREMENT_PRE			0
-#define LAB3_ITER_INCREMENT_POST		0
-#define LAB3_ITER_DECREMENT_PRE			0
-#define LAB3_ITER_DECREMENT_POST		0
-#define LAB3_ITER_DEREFERENCE			0
-#define LAB3_INSERT_EMPTY				0
-#define LAB3_INSERT_HEAD				0
-#define LAB3_INSERT_MIDDLE				0
-#define LAB3_ERASE_EMPTY				0
-#define LAB3_ERASE_HEAD					0
-#define LAB3_ERASE_TAIL					0
-#define LAB3_ERASE_MIDDLE				0
-#define LAB3_ASSIGNMENT_OP				0
-#define LAB3_COPY_CTOR					0
+#define LAB3_CTOR						1
+#define LAB3_NODE_CTOR_DEFAULT			1
+#define LAB3_NODE_CTOR					1
+#define LAB3_ADDHEAD_EMPTY				1
+#define LAB3_ADDHEAD					1
+#define LAB3_ADDTAIL_EMPTY				1
+#define LAB3_ADDTAIL					1
+#define LAB3_CLEAR						1
+#define LAB3_DTOR						1
+#define LAB3_ITER_BEGIN					1
+#define LAB3_ITER_END					1
+#define LAB3_ITER_INCREMENT_PRE			1
+#define LAB3_ITER_INCREMENT_POST		1
+#define LAB3_ITER_DECREMENT_PRE			1
+#define LAB3_ITER_DECREMENT_POST		1
+#define LAB3_ITER_DEREFERENCE			1
+#define LAB3_INSERT_EMPTY				1
+#define LAB3_INSERT_HEAD				1
+#define LAB3_INSERT_MIDDLE				1
+#define LAB3_ERASE_EMPTY				1
+#define LAB3_ERASE_HEAD					1
+#define LAB3_ERASE_TAIL					1
+#define LAB3_ERASE_MIDDLE				1
+#define LAB3_ASSIGNMENT_OP				1
+#define LAB3_COPY_CTOR					1
 
 template<typename Type>
 class DList {
@@ -84,7 +84,9 @@ class DList {
 		// LAB3_NODE_CTOR
 		Node(const Type& _data, Node* _next = nullptr, Node* _prev = nullptr) {
 			// TODO: Implement this method according to directions in lab documentation
-
+			data = _data;
+			next = _next;
+			prev = _prev;
 		}
 	};
 
@@ -102,31 +104,45 @@ public:
 		// LAB3_ITER_INCREMENT_PRE
 		Iterator& operator++() {
 			// TODO: Implement this method according to directions in lab documentation
-
+			Iterator iter;
+			iter.mCurr = mCurr;
+			iter.mCurr = mCurr->next;
+			mCurr = iter.mCurr;
+			return *this;
 		}
 
 		// LAB3_ITER_INCREMENT_POST
 		Iterator operator++(int) {
 			// TODO: Implement this method according to directions in lab documentation
-
+			Iterator iter;
+			iter.mCurr = mCurr;
+			mCurr = mCurr->next;
+			return iter;
 		}
 
 		// LAB3_ITER_DECREMENT_PRE
 		Iterator& operator--() {
 			// TODO: Implement this method according to directions in lab documentation
-
+			Iterator iter;
+			iter.mCurr = mCurr;
+			iter.mCurr = mCurr->prev;
+			mCurr = iter.mCurr;
+			return *this;
 		}
 
 		// LAB3_ITER_DECREMENT_POST
 		Iterator operator--(int) {
 			// TODO: Implement this method according to directions in lab documentation
-
+			Iterator iter;
+			iter.mCurr = mCurr;
+			mCurr = mCurr->prev;
+			return iter;
 		}
 
 		// LAB3_ITER_DEREFERENCE
 		Type& operator*() {
 			// TODO: Implement this method according to directions in lab documentation
-
+			return mCurr->data;
 		}
 
 		bool operator != (const Iterator& _iter) const {
@@ -145,46 +161,85 @@ public:
 	// LAB3_CTOR
 	DList() {
 		// TODO: Implement this method according to directions in lab documentation
-
+		mHead = nullptr;
+		mTail = nullptr;
+		mSize = 0;
 	}
 
 	// LAB3_DTOR
 	~DList() {
 		// TODO: Implement this method according to directions in lab documentation
-
-
+		Clear();
 	}
 
 	// LAB3_COPY_CTOR
 	DList(const DList& _copy) {
 		// TODO: Implement this method according to directions in lab documentation
-
+		mHead = nullptr;
+		mTail = nullptr;
+		mSize = 0;
+		for (Iterator iter = _copy.Begin(); iter != _copy.End(); ++iter) {
+			AddTail(*iter);
+		}
 	}
 
 	// LAB3_ASSIGNMENT_OP
 	DList& operator=(const DList& _assign) {
 		// TODO: Implement this method according to directions in lab documentation
-
+		if (this == &_assign) {
+			return *this;
+		}
+		Clear();
+		for (Iterator iter = _assign.Begin(); iter != _assign.End(); ++iter) {
+			AddTail(*iter);
+		}
+		return *this;
 	}
 
 	// LAB3_ADDHEAD_EMPTY
 	// LAB3_ADDHEAD
 	void AddHead(const Type& _data) {
 		// TODO: Implement this method according to directions in lab documentation
-
+		Node* tempHead = new Node(_data);
+		tempHead->next = mHead;
+		if (mHead != nullptr) {
+			mHead->prev = tempHead;
+		}
+		mHead = tempHead;
+		if (mTail == nullptr) {
+			mTail = mHead;
+		}
+		mSize++;
 	}
 
 	// LAB3_ADDTAIL_EMPTY
 	// LAB3_ADDTAIL
 	void AddTail(const Type& _data) {
 		// TODO: Implement this method according to directions in lab documentation
-
+		Node* tempTail = new Node(_data);
+		tempTail->prev = mTail;
+		if (mTail != nullptr) {
+			mTail->next = tempTail;
+		}
+		mTail = tempTail;
+		if (mHead == nullptr) {
+			mHead = mTail;
+		}
+		mSize++;
 	}
 
 	// LAB3_CLEAR
 	void Clear() {
 		// TODO: Implement this method according to directions in lab documentation
-
+		Node* temp = mHead;
+		while (temp != nullptr) {
+			Node* nextNode = temp->next;
+			delete temp;
+			temp = nextNode;
+		}
+		mHead = nullptr;
+		mTail = nullptr;
+		mSize = 0;
 	}
 
 	// LAB3_INSERT_EMPTY
@@ -192,7 +247,32 @@ public:
 	// LAB3_INSERT_MIDDLE
 	Iterator Insert(Iterator& _iter, const Type& _data) {
 		// TODO: Implement this method according to directions in lab documentation
+		Iterator iter;
+		if (mSize == 0) {
+			AddHead(_data);
+			iter.mCurr = mHead;
+			return iter;
+		}
+		else if (_iter.mCurr == mHead) {
+			AddHead(_data);
+			iter.mCurr = mHead;
+			return iter;
+		}
+		else {
+			Node* currentNode = _iter.mCurr;
+			Node* previousNode = currentNode->prev;
+			Node* newNode = new Node(_data, currentNode, previousNode);
 
+			previousNode->next = newNode;
+			newNode->prev = previousNode;
+			newNode->next = currentNode;
+			currentNode->prev = newNode;
+
+			++mSize;
+			iter.mCurr = newNode;
+			_iter.mCurr = newNode;
+			return _iter;
+		}
 	}
 
 	// LAB3_ERASE_EMPTY
@@ -201,18 +281,52 @@ public:
 	// LAB3_ERASE_MIDDLE
 	Iterator Erase(Iterator& _iter) {
 		// TODO: Implement this method according to directions in lab documentation
+		Iterator iter;
+		if (_iter.mCurr == nullptr) {
+			iter.mCurr = nullptr;
+			_iter.mCurr = nullptr;
+			return _iter;
+		}
 
+		Node* currentNode = _iter.mCurr;
+		Node* previousNode = currentNode->prev;
+		Node* nextNode = currentNode->next;
+
+		if (previousNode != nullptr) {
+			previousNode->next = nextNode;
+		}
+		else {
+			mHead = nextNode;
+		}
+
+		if (nextNode != nullptr) {
+			nextNode->prev = previousNode;
+		}
+		else {
+			mTail = previousNode;
+		}
+
+		iter.mCurr = nextNode;
+		_iter.mCurr = nextNode;
+
+		delete currentNode;
+		--mSize;
+		return _iter;
 	}
 
 	// LAB3_ITER_BEGIN
 	Iterator Begin() const {
 		// TODO: Implement this method according to directions in lab documentation
-
+		Iterator iter;
+		iter.mCurr = mHead;
+		return iter;
 	}
 
 	// LAB3_ITER_END
 	Iterator End() const {
 		// TODO: Implement this method according to directions in lab documentation
-
+		Iterator iter;
+		iter.mCurr = nullptr;
+		return iter;
 	}
 };
